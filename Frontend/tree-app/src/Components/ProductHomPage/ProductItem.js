b8a88f650f20902fa3520f98b69fe72430a49531
trandinhdanh <dinhdanh183@gmail.com>
2023-06-03 11:00:31 +0700

    handel-sold out

--- Frontend/tree-app/src/Components/ProductHomPage/ProductItem.js
+++ Frontend/tree-app/src/Components/ProductHomPage/ProductItem.js
@@ -1,60 +1,92 @@
-import React, { useEffect, useState } from 'react'
-import { Image, message } from 'antd';
-import "./ProductHomPage.scss"
-import {BsCartPlus} from "react-icons/bs"
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { openNotificationIcon } from '../NotificationIcon/NotificationIcon';
-import { cartService } from '../../services/cartService';
-import { localStorageService } from '../../services/localStorageService';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../Redux/cart/cartSlice';
+import React, { useEffect, useState } from "react";
+import { Badge, Card, Image, message } from "antd";
+import "./ProductHomPage.scss";
+import { BsCartPlus } from "react-icons/bs";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { openNotificationIcon } from "../NotificationIcon/NotificationIcon";
+import { cartService } from "../../services/cartService";
+import { localStorageService } from "../../services/localStorageService";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../Redux/cart/cartSlice";
+import { useTranslation } from "react-i18next";
 export default function ProductItem(props) {
   const [addingToCart, setAddingToCart] = useState(false);
-
+  const {t} = useTranslation()
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [user, setUser] = useState()
-  useEffect(() => { 
+  const [user, setUser] = useState();
+  useEffect(() => {
     if (props.isLoggedIn) {
       const user = localStorageService.get("USER");
       setUser(user);
     }
-   },[props.isLoggedIn])
+  }, [props.isLoggedIn]);
   const handleClick = async () => {
     if (props.isLoggedIn && user.roles[0] === "USER") {
-        setAddingToCart(true);
+      setAddingToCart(true);
 
-        const productID = props.data.id;
+      const productID = props.data.id;
 
-        await dispatch(addToCart({ userId: user.userDTO.id, productId: productID, quantity: 1 }));
+      await dispatch(
+        addToCart({
+          userId: user.userDTO.id,
+          productId: productID,
+          quantity: 1,
+        })
+      );
 
-        setAddingToCart(false);
-      
+      setAddingToCart(false);
     } else {
-      message.error('Please log USER in to add products to the cart');
+      message.error("Please log USER in to add products to the cart");
       setTimeout(() => {
-        navigate('/login');
+        navigate("/login");
       }, 3000);
     }
   };
-  return (
-    <div className='overflow-hidden w-full h-full productItem my-3 hover:shadow-md transition-all rounded-lg'>
-        <div className='productImg relative w-full'>
-             <Image  style={{ height: '300px', width: '100%', objectFit: 'cover' }} src={`${props.data.image}`} className='object-cover'/>
-             <button
-      onClick={handleClick}
-      className={`productItemIcon absolute bottom-0 left-[10px] p-5 text-primary hover:scale-125 ${addingToCart ? 'disabled' : ''}`}
-      disabled={addingToCart}
-    >
-      <BsCartPlus className="text-[20px]" />
-    </button>
+  const displayItem = () => {
+    return (
+      <div className="overflow-hidden w-full h-full productItem my-3 hover:shadow-md transition-all rounded-lg">
+        <div className="productImg relative w-full">
+          <Image
+            style={{ height: "300px", width: "100%", objectFit: "cover" }}
+            src={`${props.data.image}`}
+            className="object-cover"
+          />
+          {props.data.quantity > 0 && (
+            <button
+              onClick={handleClick}
+              className={`productItemIcon absolute bottom-0 left-[10px] p-5 text-primary hover:scale-125 ${
+                addingToCart ? "disabled" : ""
+              }`}
+              disabled={addingToCart}
+            >
+              <BsCartPlus className="text-[20px]" />
+            </button>
+          )}
         </div>
         <Link to={`/product/${props.data.id}`}>
-        <div className='productContent text-center my-3 font-roboto'>
-            <h1 className='capitalize text-[13px]'>{props.data.name}</h1>
-            <h1 className='text-[13px] font-bold'>{props.data.price.toLocaleString('vi-VN', {style: 'currency', currency: 'VND'})}</h1> 
-        </div>
+          <div className="productContent text-center my-3 font-roboto">
+            <h1 className="capitalize text-[13px]">{props.data.name}</h1>
+            <h1 className="text-[13px] font-bold">
+              {props.data.price.toLocaleString("vi-VN", {
+                style: "currency",
+                currency: "VND",
+              })}
+            </h1>
+          </div>
         </Link>
-    </div>
-  )
+      </div>
+    );
+  };
+  return (
+    <>
+      {props.data.quantity === 0 ? (
+        <Badge.Ribbon text={t('Sold Out')} color="volcano">
+          {displayItem()}
+        </Badge.Ribbon>
+      ) : (
+        displayItem()
+      )}
+    </>
+  );
 }
--- Frontend/tree-app/src/Page/Manager/OderManagePage/OderManagerPage.js
+++ Frontend/tree-app/src/Page/Manager/OderManagePage/OderManagerPage.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Modal, Table, Tag } from 'antd';
-import { orderService } from '../../../services/orderService';
-import { localStorageService } from '../../../services/localStorageService';
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Table, Tag } from "antd";
+import { orderService } from "../../../services/orderService";
+import { localStorageService } from "../../../services/localStorageService";
 
 export default function OderManagerPage() {
   const [orders, setOrders] = useState([]);
-  const [idUser, setIdUser] = useState(localStorageService.get('USER').userDTO.id);
+  const [idUser, setIdUser] = useState(
+    localStorageService.get("USER").userDTO.id
+  );
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState(null);
   useEffect(() => {
     fetchOrders();
   }, [idUser]);
 
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  const handleUpdate = (record) => { 
+  const handleUpdate = (record) => {
+    setSelectedOrder(record);
     setIsModalOpen(true);
-   }
+  };
   const fetchOrders = async () => {
     try {
       const response = await orderService.getOrderBySeller(idUser);
@@ -32,52 +33,52 @@ export default function OderManagerPage() {
 
   const getStatusColor = (status) => {
     switch (status) {
-      case 'ORDER_CONFIRM':
-        return 'blue';
-      case 'ORDER_WAIT_CONFIRM':
-        return 'yellow';
-      case 'ORDER_DONE':
-        return 'green';
+      case "ORDER_CONFIRM":
+        return "blue";
+      case "ORDER_WAIT_CONFIRM":
+        return "yellow";
+      case "ORDER_DONE":
+        return "green";
       default:
-        return 'red';
+        return "red";
     }
   };
-  
+
   const columns = [
     {
-      title: 'Order ID',
-      dataIndex: 'id',
-      key: 'id',
+      title: "Order ID",
+      dataIndex: "id",
+      key: "id",
     },
     {
-      title: 'Created By',
-      dataIndex: 'createBy',
-      key: 'createBy',
+      title: "Created By",
+      dataIndex: "createBy",
+      key: "createBy",
     },
     {
-      title: 'Payment Method',
-      dataIndex: 'paymentMethod',
-      key: 'paymentMethod',
+      title: "Payment Method",
+      dataIndex: "paymentMethod",
+      key: "paymentMethod",
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
       render: (status) => <Tag color={getStatusColor(status)}>{status}</Tag>,
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
     },
     {
-      title: 'Phone Number',
-      dataIndex: 'numberPhone',
-      key: 'numberPhone',
+      title: "Phone Number",
+      dataIndex: "numberPhone",
+      key: "numberPhone",
     },
     {
-      title: 'Actions',
-      key: 'actions',
+      title: "Actions",
+      key: "actions",
       render: (text, record) => (
         <Button type="primary" onClick={() => handleUpdate(record)}>
           Update
@@ -85,11 +86,40 @@ export default function OderManagerPage() {
       ),
     },
   ];
-
+  const handleConfirm = async (idOrder) => {
+    try {
+      const response = await orderService.confirm(idOrder);
+      console.log(response);
+      setOrders(response.data);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleDone = async (idOrder) => {
+    try {
+      const response = await orderService.done(idOrder);
+      console.log(response);
+      setOrders(response.data);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleCancelOrder = async (idOrder) => {
+    try {
+      const response = await orderService.cancel(idOrder);
+      console.log(response);
+      setOrders(response.data);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const tableStyle = {
-    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
-    borderRadius: '8px',
-    overflow: 'hidden',
+    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+    borderRadius: "8px",
+    overflow: "hidden",
   };
 
   return (
@@ -106,30 +136,62 @@ export default function OderManagerPage() {
               <ul>
                 {record.orderDetails.map((detail) => (
                   <li key={detail.id}>
-                    Name Product: {detail.product.name} - Quantity: {detail.quantity} - Price: {detail.price.toLocaleString('vi-VN', {style: 'currency', currency: 'VND'})}
+                    Name Product: {detail.product.name} - Quantity:{" "}
+                    {detail.quantity} - Price:{" "}
+                    {detail.price.toLocaleString("vi-VN", {
+                      style: "currency",
+                      currency: "VND",
+                    })}
                   </li>
                 ))}
                 <li key={record.id}>
-                   Total Price: {record.totalPrice.toLocaleString('vi-VN', {style: 'currency', currency: 'VND'})}
-                  </li>
+                  Total Price:{" "}
+                  {record.totalPrice.toLocaleString("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                  })}
+                </li>
               </ul>
             ),
             rowExpandable: (record) => record.orderDetails.length > 0,
           }}
         />
       </div>
-      <Modal title="Update Status Order" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-      <div className='flex flex-col space-y-2 mx-20 py-5'>
-      <Button type="primary" style={{ background: '#E8AA42' }}>
-             ORDER_CONFIRM
-        </Button>
-        <Button type="primary" style={{ background: '#1B9C85' }}>
-             ORDER_DONE
-        </Button>
-        <Button type="primary" style={{ background: '#B70404' }}>
-             ORDER_CANCEL  
-        </Button>
-      </div>
+      <Modal
+        title="Update Status Order"
+        visible={isModalOpen}
+        onCancel={handleCancel}
+        footer={
+          <div>
+            <Button type="primary" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </div>
+        }
+      >
+        <div className="flex flex-col space-y-2 mx-20 py-5">
+          <Button
+            type="primary"
+            style={{ background: "#E8AA42" }}
+            onClick={() => handleConfirm(selectedOrder.id)}
+          >
+            ORDER_CONFIRM
+          </Button>
+          <Button
+            type="primary"
+            style={{ background: "#1B9C85" }}
+            onClick={() => handleDone(selectedOrder.id)}
+          >
+            ORDER_DONE
+          </Button>
+          <Button
+            type="primary"
+            style={{ background: "#B70404" }}
+            onClick={() => handleCancelOrder(selectedOrder.id)}
+          >
+            ORDER_CANCEL
+          </Button>
+        </div>
       </Modal>
     </div>
   );
